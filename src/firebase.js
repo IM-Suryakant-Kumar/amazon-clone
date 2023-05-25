3eee0c8762a180abdec1118aa0cdad716988fdd1
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-05-25 13:44:51 +0530

    feat: Header Home And Product component added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArHMjxL_40Z2VDjY0Znq4V-3QSYGZKXKM",
+  authDomain: "clone-c6080.firebaseapp.com",
+  projectId: "clone-c6080",
+  storageBucket: "clone-c6080.appspot.com",
+  messagingSenderId: "480540112486",
+  appId: "1:480540112486:web:004f7a0c14288384319fa7",
+  measurementId: "G-DL2TP8DQ2P"
+};
